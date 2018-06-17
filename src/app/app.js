4194aefbbb72f2233f8fde687a181d255dae94ac
Ivan Tarasov <guser999@mail.ru>
2018-06-18 01:26:17 +0700

    сделал основной функционал, остался status

--- src/app/app.js
+++ src/app/app.js
@@ -2,5 +2,19 @@
 var searchlinkApp = angular.module('searchlinkApp', []);
 
 searchlinkApp.controller('SearchLinkController', function SearchLinkController($scope) {
-  $scope.array = ['Bed', 'Sad', 'Glad'];
+  $scope.getData = function () {
+      $scope.array = $scope.data.split('\n');
+  };
+  $scope.clearDomainList = function () {
+      $scope.data = "";
+  };
+  $scope.printFinalList = function() {
+      $scope.finalList = "";
+      $scope.array.forEach(item => {
+          $scope.finalList = $scope.finalList + item + '\n';
+      });
+  };
+  $scope.removeDomain = function (domain) {
+      $scope.array.splice([$scope.array.indexOf(domain)], 1);
+  };
 });
